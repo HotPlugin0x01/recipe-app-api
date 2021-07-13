f753353fb7c01aa74cddd2c3cde25f0a264dfa60
Abdul Wassay <wassaysardar35@gmail.com>
2021-07-13 19:55:53 +0500

    Add test for email casing

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -15,3 +15,14 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user"""
+        email = 'test@GMAIL.com'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password='test123'
+        )
+
+        self.assertEqual(user.email, email.lower())
